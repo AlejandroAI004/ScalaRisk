96b816fb551ca48ad3f5ff5f8b45a3630607eef6
alejandro <alejandropgde@gmail.com>
2025-11-28 00:05:01 +0100

    coverage tests...

--- src/test/scala/Tests/gameState_spec.scala
+++ src/test/scala/Tests/gameState_spec.scala
@@ -7,17 +7,58 @@ import org.scalatest.matchers.should.Matchers
 class gameState_spec extends AnyWordSpec with Matchers {
   "PlacementState" should {
 
-    "switch to OffenseState when all players have no infantry left" in {
-      val p1 = new player("red")
-      p1.infantry = 0
-      val p2 = new player("blue")
-      p2.infantry = 0
-      val players = List(p1, p2)
+    "have name \"Placement\"" in {
+      PlacementState.name shouldBe "Placement"
+    }
 
+    "stay in PlacementState on AttackEvent" in {
+      val p1      = new player("red")
+      val players = List(p1)
       val mapData = MapInit.testMap_init()
-      val controller = new GameController(mapData, players, SimpleCombatStrategy)
+      val ctrl    = new GameController(mapData, players, SimpleCombatStrategy)
 
-      val next = PlacementState.handle(controller, players, PlaceInfantryEvent)
+      val next = PlacementState.handle(ctrl, players, AttackEvent)
+
+      next shouldBe PlacementState
+    }
+
+    "switch to OffenseState on PlaceInfantryEvent when all players have no infantry left" in {
+      val p1      = new player("red");  p1.infantry = 0
+      val p2      = new player("blue"); p2.infantry = 0
+      val players = List(p1, p2)
+      val mapData = MapInit.testMap_init()
+      val ctrl    = new GameController(mapData, players, SimpleCombatStrategy)
+
+      val next = PlacementState.handle(ctrl, players, PlaceInfantryEvent)
+
+      next shouldBe OffenseState
+    }
+  }
+
+  "OffenseState" should {
+
+    "have name \"Offense\"" in {
+      OffenseState.name shouldBe "Offense"
+    }
+
+    "stay in OffenseState on AttackEvent" in {
+      val p1      = new player("red")
+      val players = List(p1)
+      val mapData = MapInit.testMap_init()
+      val ctrl    = new GameController(mapData, players, SimpleCombatStrategy)
+
+      val next = OffenseState.handle(ctrl, players, AttackEvent)
+
+      next shouldBe OffenseState
+    }
+
+    "stay in OffenseState on PlaceInfantryEvent" in {
+      val p1      = new player("red")
+      val players = List(p1)
+      val mapData = MapInit.testMap_init()
+      val ctrl    = new GameController(mapData, players, SimpleCombatStrategy)
+
+      val next = OffenseState.handle(ctrl, players, PlaceInfantryEvent)
 
       next shouldBe OffenseState
     }
